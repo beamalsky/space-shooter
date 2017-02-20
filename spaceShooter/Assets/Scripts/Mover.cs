63614b9e89ca7ed9584e28fd1561978ff54691ab
beamalsky <beamalsky@gmail.com>
2017-02-20 16:59:04 -0600

    Through "creating shots"

---
+++ spaceShooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	void Start () {
+		
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3 (0.0f, 0.0f, speed);
+	}
+}
---
+++ spaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;	
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float tilt;
+	public float speed;
+	public Boundary boundary;
+	 
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = speed * movement;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+
+}
