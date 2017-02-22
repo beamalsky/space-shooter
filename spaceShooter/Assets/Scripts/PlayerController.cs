11fd8c810c6d1b80a510f16a4379a8d91ebbe5fe
beamalsky <beamalsky@gmail.com>
2017-02-22 17:54:45 -0600

    Shots fired

--- spaceShooter/Assets/Scripts/PlayerController.cs
+++ spaceShooter/Assets/Scripts/PlayerController.cs
@@ -13,9 +13,21 @@ public class PlayerController : MonoBehaviour {
 	public float tilt;
 	public float speed;
 	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
 	 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);  
+		}
 	}
 
 	void FixedUpdate () {
